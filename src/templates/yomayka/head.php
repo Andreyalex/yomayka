98161a4681787812670cc5b129a1e0ecee4a333f
Andrey <dev.andreyalek@gmail.com>
2014-10-12 02:14:33 +0300

    Added current path

--- src/templates/yomayka/head.php
+++ src/templates/yomayka/head.php
@@ -4,7 +4,10 @@
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <meta charset="utf-8" >
 
-    <script type="text/javascript"> window.siteBaseUrl = "<?php echo JUri::base(); ?>"</script>
+    <script type="text/javascript">
+        window.siteBaseUrl = "<?php echo JUri::base(); ?>"
+        window.siteCurrentUrl = "<?php echo JUri::current(); ?>"
+    </script>
 
     <!--    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> -->
     <link rel="stylesheet" href="<?php echo $tplBaseUrl; ?>/assets/bootstrap/css/bootstrap.min.css">
